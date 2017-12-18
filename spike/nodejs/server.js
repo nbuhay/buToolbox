8ef66c42613c44da60d713209aeb5cde16b0e5af
Nick Buhay <nbuhay@gmail.com>
2017-12-18 10:00:31 -0600

    Add sample nodejs spike project

---
+++ spike/nodejs/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  var date = Date.now();
+  console.log(date + ': Request recieved by server');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(date + ': Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
