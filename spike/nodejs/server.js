3fe654794ac7f741ec82b2ce473d430de2c06018
Nick Buhay <nbuhay@gmail.com>
2017-12-18 10:23:33 -0600

    Edit nodejs server

--- spike/nodejs/server.js
+++ spike/nodejs/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   var date = Date.now();
-  console.log(date + ': Request recieved by server');
+  console.log(date + ': Request received by server');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(date + ': Hello World!\n');
