f61b37f6fca3d5b1aba55d59264c581da76184a2
Nick Buhay <nbuhay@gmail.com>
2017-12-22 03:15:43 -0600

    Refactor Python Flask app with verbose output for test route.

--- spike/python/flask/app.py
+++ spike/python/flask/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from datetime import datetime
+from socket import gethostname, gethostbyname
 app = Flask(__name__)
 port=8080
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  print gethostbyname(gethostname())
+  return str(datetime.now()) + ": " + gethostname() + ": Hello World from Python Flask!"
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=port)
