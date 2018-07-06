d76993261b44b4c4e45cae8dd9b64996ed738072
Nick Buhay <nbuhay@gmail.com>
2018-07-06 17:11:49 -0500

    Update Ubuntu kubectl install.

--- linux/ubuntu/install/kubectl.sh
+++ linux/ubuntu/install/kubectl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-binary-via-curl
-sudo apt-get update
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
