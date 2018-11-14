129c04be5e2f4f696d790fc7d26b2747e8bd21e1
Nick Buhay <nbuhay@gmail.com>
2018-11-13 19:57:50 -0600

    Update linux ubuntu install node.js script.

--- linux/ubuntu/install/nodejs.sh
+++ linux/ubuntu/install/nodejs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install Node Version Manager
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+# https://github.com/creationix/nvm
+curl https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
