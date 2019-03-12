648321cb1455a7bae16f50ba0cec60cecf18f3b1
Nick Buhay <nbuhay@gmail.com>
2019-03-11 23:03:17 -0500

    Refactor spike nodejs server.js.

--- spike/nodejs/server.js
+++ spike/nodejs/server.js
@@ -1,16 +1,18 @@
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 3000;
+const http = require('http')
+    , os = require('os')
+    , port = 3000
+    , hostname = os.hostname()
 
 const server = http.createServer((req, res) => {
   var date = new Date();
   console.log(date + ': Request received by server');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(date + ': Hello World from Node.js server!\n');
+  res.end(`Date:     ${date}\n` +
+          `Hostname: ${hostname}\n` +
+          `Message:  Hello World from Node.js server!`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
