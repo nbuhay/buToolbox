9e166471eee1a0e4e7e70e864caa7adf68c9d535
Nick Buhay <nbuhay@gmail.com>
2019-03-11 23:54:40 -0500

    Refactor spike golang main.go app.

--- spike/golang/main.go
+++ spike/golang/main.go
@@ -4,17 +4,35 @@ import (
   "net/http"
   "time"
   "os"
+  "encoding/json"
+  "fmt"
 )
+
+type response struct {
+  Date string `json:date`
+  Hostname string `json:hostname`
+  Client string `json:client`
+  Msg string `json:msg`
+}
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
   hostname, err := os.Hostname()
   if err != nil {
     panic(err)
   }
-  message := "Date:      " + time.Now().String() +
-             "\nHostname:  " + hostname +
-             "\nMessage:   Hello from Golang!"
-  w.Write([]byte(message))
+  date := time.Now().String()
+  client := r.RemoteAddr
+  msg := `Hello from Golang!`
+
+  resPayload := &response{
+    Date: date,
+    Hostname: hostname,
+    Client: client,
+    Msg: msg}
+
+  fmt.Printf(date + ": hostname " + hostname + ": client " +  client +"\n")
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(resPayload)
 }
 
 func main() {
