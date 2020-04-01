a6c72e4cf0b4a32279c99468381b1af1025f2732
Nic Buhay <nbuhay@gmail.com>
2020-04-01 13:14:21 -0500

    Update linux install node.js version.

--- linux/ubuntu/install/nodejs.sh
+++ linux/ubuntu/install/nodejs.sh
@@ -2,7 +2,7 @@
 
 # Install Node Version Manager
 # https://github.com/creationix/nvm
-curl https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
